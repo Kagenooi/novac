53b32c411a68f2328832dc462a4a042a6c929af1
kageno <kagenooi.morpheus@gmail.com>
2024-11-07 06:40:11 +0400

    add new pages

---
+++ js/client/client-contragent.js
@@ -0,0 +1,28 @@
+import {
+    adaptive
+} from "../../adaptiveMode/adaptive.js";
+window.adaptive = adaptive;
+adaptive();
+window.addEventListener('resize', function () { adaptive(); });
+
+import {
+    placeholderAnimate,
+    addFileLabel,
+    transactionTabs,
+    toggleActive,
+    chooseOption
+} from "../module.js";
+
+window.toggleActive = toggleActive;
+window.chooseOption = chooseOption;
+
+document.querySelector('#defaultOption').click();
+
+window.addFileLabel = addFileLabel;
+addFileLabel();
+
+window.placeholderAnimate = placeholderAnimate;
+placeholderAnimate();
+
+window.transactionTabs = transactionTabs;
+transactionTabs();
