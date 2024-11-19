cb6b59a5bf64b2b9fe25e9e77bf090c580054ae3
kageno <kagenooi.morpheus@gmail.com>
2024-11-19 19:46:26 +0400

    fv

--- js/client/client-transactions.js
+++ js/client/client-transactions.js
@@ -26,3 +26,17 @@ placeholderAnimate();
 
 window.transactionTabs = transactionTabs;
 transactionTabs();
+
+const orderDetailsBtns = document.querySelectorAll('.orderDetails__info_btns_btn');
+const orderDetailsTabs = document.querySelectorAll('.orderDetails__info_tab');
+for (let i = 0; i < orderDetailsBtns.length; i++) {
+    orderDetailsBtns[i].addEventListener('click', function() {
+        for (let x = 0; x < orderDetailsTabs.length; x++) {
+            orderDetailsTabs[x].classList.remove('active');
+            orderDetailsBtns[x].classList.remove('active');
+        }
+        document.querySelector(`#${this.dataset.tab}`).classList.add('active');
+        this.classList.add('active');
+    })
+}
+document.querySelector('#defaultTab').click();
