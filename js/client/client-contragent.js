d34b37e0785b9bba1695cf9e3e32ff854882880a
kageno <kagenooi.morpheus@gmail.com>
2024-11-20 04:34:12 +0400

    erty

--- js/client/client-contragent.js
+++ js/client/client-contragent.js
@@ -39,4 +39,24 @@ for (let i = 0; i < contragentDetailsBtns.length; i++) {
         this.classList.add('active');
     })
 }
+document.querySelector('#defaultContragentTab').click();
+
+const orderDetailsBtns = document.querySelectorAll('.orderDetails__info_btns_btn');
+const orderDetailsTabs = document.querySelectorAll('.orderDetails__info_tab');
+for (let i = 0; i < orderDetailsBtns.length; i++) {
+    orderDetailsBtns[i].addEventListener('click', function() {
+        for (let x = 0; x < orderDetailsTabs.length; x++) {
+            orderDetailsTabs[x].classList.remove('active');
+            orderDetailsBtns[x].classList.remove('active');
+        }
+        document.querySelector(`#${this.dataset.tab}`).classList.add('active');
+        this.classList.add('active');
+    })
+}
 document.querySelector('#defaultTab').click();
+
+const orderStatusWrapper = document.querySelector('.orderDetails__status');
+const orderStatusEl = orderStatusWrapper.querySelectorAll('.orderDetails__status_el_name');
+for (let i = 0; i < orderStatusEl.length; i++) {
+    orderStatusEl[i].nextElementSibling.style.background = orderStatusEl[i].dataset.bg;
+}
